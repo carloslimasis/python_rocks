32ba42f22417cd4cf010d1118263908e180d10f0
carloslimasis <carloslimasis@gmail.com>
2017-02-26 15:50:02 -0300

    Added files

---
+++ Working with Lists/working_with_lists.py
@@ -0,0 +1,21 @@
+relatives = []
+print relatives
+
+relatives = ['Rebeca', 'John', 'Eddie']
+print relatives
+
+ages = [18,20,14,22,35,32]
+print ages[3]
+
+invitations = ['Romulo', 'Almeida', 'Bronson', 'Nicodemos', 'Rubi']
+print invitations[2:4]
+
+ages = [20,29,12,59,41]
+print ages
+ages.append(25)
+print ages
+
+varied = ['A', 'B', 12, 'F', 14]
+print varied
+varied.remove('B')
+print varied
